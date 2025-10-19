f0278889ac6748e6276213db6e8ec7d6dcd7b4b0
Mingyu Kim <mattmingyu@gmail.com>
2025-10-19 23:55:50 +0900

    Update 20251018_jun_14681.py

--- 20251018_jun_14681.py
+++ 20251018_jun_14681.py
@@ -9,3 +9,11 @@ elif x < 0 and y < 0 :
     print('3')
 else:
     print('4')
+
+# 10871
+n, x = map(int, input().split())
+num = list(map(int, input().split()))
+
+for i in range(n):
+    if num[i] < x:
+        print(num[i], end = " ")
