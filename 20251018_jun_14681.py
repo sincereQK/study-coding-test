9bc89372e39d744ad76b7c97dbd0219f77a2ae49
Mingyu Kim <mattmingyu@gmail.com>
2025-10-20 21:28:32 +0900

    Update 20251018_jun_14681.py

--- 20251018_jun_14681.py
+++ 20251018_jun_14681.py
@@ -17,3 +17,18 @@ num = list(map(int, input().split()))
 for i in range(n):
     if num[i] < x:
         print(num[i], end = " ")
+
+# 1316
+N = int(input())
+cnt = N
+
+for i in range(N):
+    word = input()
+    for j in range(0, len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+
+print(cnt)
