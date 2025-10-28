84068c6579001141b6d6d9ba00e15c0d52e590c4
Mingyu Kim <mattmingyu@gmail.com>
2025-10-28 23:31:44 +0900

    Update 20251018_jun_14681.py

--- 20251018_jun_14681.py
+++ 20251018_jun_14681.py
@@ -102,3 +102,13 @@ myInput = list(map(int, input().split()))
 
 for i in range(len(chess)):
   print(chess[i] - myInput[i], end = ' ')
+
+#2444
+N = int(input())
+
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+    
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+
