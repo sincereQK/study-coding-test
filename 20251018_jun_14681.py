83ca6eff5c225a2bf82cbb6b3fb641ca41bf8bdb
Mingyu Kim <mattmingyu@gmail.com>
2025-11-02 22:08:11 +0900

    Update 20251018_jun_14681.py

--- 20251018_jun_14681.py
+++ 20251018_jun_14681.py
@@ -147,5 +147,27 @@ nums_pileup = 1
 cnt = 1
 while n > nums_pileup :
     nums_pileup += 6 * cnt
+
+#1193
+N = int(input())
+
+line = 0
+line_end_num = 0
+
+while line_end_num < N:
+    line += 1
+    line_end_num += line
+
+K = line_end_num - N
+
+if line % 2 == 0:
+    a = line - K
+    b = K + 1
+else:
+    a = K + 1
+    b = line - K
+
+print(str(a) + '/' + str(b))
     cnt += 1
 print(cnt)
+
