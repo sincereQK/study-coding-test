22f306b769743e8f6a822592e0dd1fd60933ac23
Mingyu Kim <mattmingyu@gmail.com>
2025-11-06 23:56:54 +0900

    Update 20251018_jun_14681.py

--- 20251018_jun_14681.py
+++ 20251018_jun_14681.py
@@ -192,3 +192,25 @@ if len(sosu_list) > 0 :
 else:
     print(-1)
 
+#2581
+M = int(input())
+N = int(input())
+
+sosu_list = []
+for num in range(M, N+1):
+    count = 0 
+    if num > 1:
+        for i in range(2, num): 
+            if num % i == 0: 
+                count += 1
+                break
+        if count == 0: 
+            sosu_list.append(num)
+
+
+if len(sosu_list) > 0:
+    print(sum(sosu_list))
+    print(min(sosu_list))
+else:
+    print(-1)
+
