c12a45631ecd665c0e91890ea374bb31fba826eb
Mingyu Kim <mattmingyu@gmail.com>
2025-11-18 23:56:13 +0900

    Update 20251018_jun_14681.py

--- 20251018_jun_14681.py
+++ 20251018_jun_14681.py
@@ -324,3 +324,16 @@ for i in range(n-7):
         result.append(draw2)
  
 print(min(result))
+
+#1436
+n = int(input())
+cnt = 0
+result = 666
+ 
+while True:
+    if '666' in str(result):
+        cnt += 1
+    if cnt == n:
+        break
+    result += 1
+print(result)
