b5064f617c6eb18b9c004ca9bb09e64daae3ed7c
Mingyu Kim <mattmingyu@gmail.com>
2025-11-20 23:17:24 +0900

    Update 20251018_jun_14681.py

--- 20251018_jun_14681.py
+++ 20251018_jun_14681.py
@@ -353,3 +353,14 @@ while num >= 0:
   
 else:
   print(-1)
+
+#2750
+N = int(input())
+num = []
+
+for i in range(N):
+    num.append(int(input()))
+num2 = sorted(num)
+
+for i in range(len(num)):
+    print(num2[i])
