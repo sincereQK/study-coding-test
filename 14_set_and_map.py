db9cba7184f6d2b69e0f8b16875260db5f4c630d
Mingyu Kim <mattmingyu@gmail.com>
2025-11-27 23:58:01 +0900

    Update and rename set_and_map.py to 14_set_and_map.py

--- 14_set_and_map.py
+++ 14_set_and_map.py
@@ -14,4 +14,19 @@ for card in check_list:
         print(1, end=" ")
     else:
         print(0, end=" ")
-      
+
+#14425
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+S = set()
+for i in range(N):
+    S.add(input())
+ans = 0
+for _ in range(M):
+    t = input()
+    if t in S:
+        ans+=1
+print(ans)
+
+#
