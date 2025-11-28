75e35d1ea45be07cd8534bc2f9e0728775c9293a
Mingyu Kim <mattmingyu@gmail.com>
2025-11-28 23:49:34 +0900

    Update 14_set_and_map.py

--- 14_set_and_map.py
+++ 14_set_and_map.py
@@ -29,4 +29,22 @@ for _ in range(M):
         ans+=1
 print(ans)
 
+#7785
+import sys
+
+n = int(sys.stdin.readline())
+temp = dict()
+
+for _ in range(n):
+    a, b = map(str, sys.stdin.readline().split())
+    if b == "enter":
+        temp[a] = b
+    else:
+        del temp[a]
+
+temp = sorted(temp.keys(), reverse=True)
+
+for i in temp:
+    print(i)
+
 #
