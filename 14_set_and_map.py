222627aa3c3e0a592d5a873d47c7e771df7c3664
Mingyu Kim <mattmingyu@gmail.com>
2025-11-29 12:08:15 +0900

    Update 14_set_and_map.py

--- 14_set_and_map.py
+++ 14_set_and_map.py
@@ -47,4 +47,23 @@ temp = sorted(temp.keys(), reverse=True)
 for i in temp:
     print(i)
 
+#1620
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+d1 = {}
+d2 = {}
+for i in range(N):
+    name = sys.stdin.readline().rstrip()
+    d1[i + 1] = name    
+    d2[name] = i + 1
+
+for _ in range(M):
+    data = sys.stdin.readline().rstrip()
+    if (data.isdecimal()):
+        print(d1[int(data)])
+    else:
+     print(d2.get(data))
+
 #
