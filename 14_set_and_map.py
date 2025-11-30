b19211dedead14d448793eb61f3bdc6ef801cae3
Mingyu Kim <mattmingyu@gmail.com>
2025-11-30 22:50:24 +0900

    Update 14_set_and_map.py

--- 14_set_and_map.py
+++ 14_set_and_map.py
@@ -66,4 +66,28 @@ for _ in range(M):
     else:
      print(d2.get(data))
 
+#10816
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+cards = [*map(int, input().split())]
+M = int(input())
+candidate = [*map(int, input().split())]
+
+count = {}
+for card in cards:
+    if card in count:
+        count[card] += 1
+    else:
+        count[card] = 1
+
+for target in candidate:
+    result = count.get(target)
+    if result == None:
+        print(0, end=" ")
+    else:
+        print(result, end=" ")
+
 #
+
