89a91eb49dc204b87ce8edf6243a194b0eb24732
Mingyu Kim <mattmingyu@gmail.com>
2025-12-02 02:15:55 +0900

    Update 14_set_and_map.py

--- 14_set_and_map.py
+++ 14_set_and_map.py
@@ -97,24 +97,30 @@ N, M = map(int, sys.stdin.readline().strip().split())
 set1 = set()
 set2 = set()
 
-# 듣도 못한 사람
 for _ in range(N):
     set1.add(sys.stdin.readline().strip())
 
-# 보도 못한 사람
 for _ in range(M):
     set2.add(sys.stdin.readline().strip())
 
-# 듣보잡들
 result = sorted(list(set1 & set2))
 
-# 듣보잡 수
 print(len(result))
 
-# 듣보잡 이름
 for x in result:
     print(x)
 
+#1269
+import sys
+input = sys.stdin.readline
+
+n_a, n_b = map(int, input().split())
+S_a = set(map(int, input().split()))
+S_b = set(map(int, input().split()))
+
+print(len(S_a - S_b) + len(S_b - S_a))
+
 #
+
 
 
