e1ddb1e16b98fa7d8b25d491325e7a440e0a714c
Mingyu Kim <mattmingyu@gmail.com>
2025-12-08 21:56:34 +0900

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,1 +1,28 @@
+#2751
+# import sys
 
+n = int(input())
+nums = []
+
+for _ in range(n):
+    nums.append(int(sys.stdin.readline()))
+
+nums.sort()
+
+for num in nums:
+    print(num)
+
+#10989
+#import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [0] * (10000 + 1)
+
+for _ in range(n):
+    arr[int(input())] += 1
+
+for i in range(len(arr)):
+    if arr[i] != 0: 
+        for _ in range(arr[i]):
+            print(i)
