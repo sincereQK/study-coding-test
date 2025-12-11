ad4f5634809c4599f5a0f8ed2cda341363390647
Mingyu Kim <mattmingyu@gmail.com>
2025-12-11 22:48:42 +0900

    Update 15_Divisors_Multiples_and_Prime_Numbers.py

--- 15_Divisors_Multiples_and_Prime_Numbers.py
+++ 15_Divisors_Multiples_and_Prime_Numbers.py
@@ -99,7 +99,27 @@ for i in range(N, M+1):
     else: # for문이 잘 실행이 되었다면 print(i) 출력
         print(i)
 
-#
+#4948
+num = 123456*2+1
+num_list = [1]*num
+for i in range(1, num):
+    if i == 1:
+        continue
+    for j in range(2, int(i**0.5)+1):
+        if i % j == 0:
+            num_list[i] = 0
+            break
+
+while True:
+    n = int(input())
+    
+    if n == 0:
+        break
+    
+    prime = 0
+    for i in range(n+1, 2*n+1):
+            prime += num_list[i]
+    print(prime)
 
 #
 
