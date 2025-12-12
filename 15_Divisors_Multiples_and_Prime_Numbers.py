34f021a7c69511681007c4b14d138185a9622b22
Mingyu Kim <mattmingyu@gmail.com>
2025-12-12 23:21:55 +0900

    Update 15_Divisors_Multiples_and_Prime_Numbers.py

--- 15_Divisors_Multiples_and_Prime_Numbers.py
+++ 15_Divisors_Multiples_and_Prime_Numbers.py
@@ -121,5 +121,29 @@ while True:
             prime += num_list[i]
     print(prime)
 
+#17103
+import sys
+
+li = list(True for _ in range(1000001))
+li[0] = False
+li[1] = False
+for i in range(2, 1000001):
+    if li[i]:
+        for j in range(i * 2, 1000001, i):
+            li[j] = False
+        
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    
+    count = 0
+    for i in range(2, N // 2 + 1):
+        if (li[i] and li[N - i]):
+            count += 1
+            
+    print(count)
+
 #
+
 
