ded975898c700103dfce29077b75a9bc76f14b01
Mingyu Kim <mattmingyu@gmail.com>
2025-12-14 21:58:34 +0900

    Update 16_ Stack_Queue_Deck_1.py

--- 16_ Stack_Queue_Deck_1.py
+++ 16_ Stack_Queue_Deck_1.py
@@ -1,5 +1,29 @@
 #28278
+import sys
 
+N = int(sys.stdin.readline()) # 명령의 수 입력 받기
+ST = [] # 스택 초기화
+for i in range(N): # 5가지의 명령에 대해서 코드 작성
+    op = sys.stdin.readline().split() # 1 42 입력하면 ['1', '42']
+    if op[0] == '1':
+        ST.append(int(op[-1]))
+    elif op[0] == '2':
+        if ST:
+            print(ST.pop(-1))
+            continue
+        print(-1)
+    elif op[0] == '3':
+        print(len(ST))
+    elif op[0] == '4':
+        if ST:
+            print(0)
+            continue
+        print(1)
+    elif op[0] == '5':
+        if ST:
+            print(ST[-1])
+            continue
+        print(-1)
 
 #
 
