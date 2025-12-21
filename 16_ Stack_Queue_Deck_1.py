673ccba69d2df178352e6aa6d284a19ca767773d
Mingyu Kim <mattmingyu@gmail.com>
2025-12-21 23:59:10 +0900

    Update 16_ Stack_Queue_Deck_1.py

--- 16_ Stack_Queue_Deck_1.py
+++ 16_ Stack_Queue_Deck_1.py
@@ -177,8 +177,24 @@ while len(card) != 1:
     
 print(card[0])
 
-#
+#11866
+from sys import stdin
 
+N, K = map(int,stdin.readline().split())
+index = 0
+array = list(range(1,N+1))
+result = []
+
+while len(array) != 0: 
+    index += (K-1)
+    index = index % len(array)
+    result.append(array.pop(index))
+
+print("<",end="")
+for i in range(N-1):
+    print(result[i],end=", ")
+print(result[N-1], end = "")
+print(">",end="")
 
 #
 
