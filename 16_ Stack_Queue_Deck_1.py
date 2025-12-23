04c08c94231740280ea61f214d43b6f9bb37721c
Mingyu Kim <mattmingyu@gmail.com>
2025-12-23 23:56:24 +0900

    Update 16_ Stack_Queue_Deck_1.py

--- 16_ Stack_Queue_Deck_1.py
+++ 16_ Stack_Queue_Deck_1.py
@@ -222,8 +222,27 @@ for i in range(n):
     elif num[0] == 8:
         print(dequeue[-1] if l else -1)
 
-#
+#2346
+import sys
+from collections import deque
 
+input = sys.stdin.readline
+
+n = int(input())
+paper = deque(list(map(int, input().split())))
+index = deque([i for i in range(1, n + 1)])
+poped = []
+
+while paper:
+    num = paper.popleft()
+    poped.append(index.popleft())
+    if num > 0:
+        paper.rotate(-(num-1))
+        index.rotate(-(num-1))
+    else:
+        paper.rotate(-num)
+        index.rotate(-num)
+print(*poped)
 
 #
 
