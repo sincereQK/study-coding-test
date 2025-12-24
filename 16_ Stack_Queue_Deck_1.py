89857b9d811bb497c75edacd85225a1586321efa
Mingyu Kim <mattmingyu@gmail.com>
2025-12-24 14:45:29 +0900

    Update 16_ Stack_Queue_Deck_1.py

--- 16_ Stack_Queue_Deck_1.py
+++ 16_ Stack_Queue_Deck_1.py
@@ -244,7 +244,23 @@ while paper:
         index.rotate(-num)
 print(*poped)
 
-#
+#24511
+import sys
+from collections import deque
 
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+B = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+C = list(map(int, sys.stdin.readline().split()))
 
+D = deque()
+for i in range(N):
+    if (A[i] == 0):
+        D.append(B[i])
 
+for i in range(M):
+    D.appendleft(C[i])
+    print(D.pop(), end=' ')
+
+#end
