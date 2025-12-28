c4e693c793e1de69863e79103b28ba39cb83c0be
Mingyu Kim <mattmingyu@gmail.com>
2025-12-28 22:54:19 +0900

    Update 17_Combination.py

--- 17_Combination.py
+++ 17_Combination.py
@@ -14,7 +14,18 @@ if n > 0:
         result *= i
 print(result)
 
-#
+#11050
+n,k = map(int,input().split())
+a = 1
+b = 1
+c = 1
 
+for _ in range(1,n+1):
+    a *= _
+for i in range(1,n-k+1):
+    b *= i
+for j in range(1,k+1):
+    c *= j
+print(int(a/(b*c)))
 
 #
