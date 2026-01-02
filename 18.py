3d4517defc3291a96add9e8dd95dde5bd9a2f600
Mingyu Kim <mattmingyu@gmail.com>
2026-01-03 00:26:33 +0900

    Update 18.py

--- 18.py
+++ 18.py
@@ -8,4 +8,25 @@ min_num = min(A)
 
 print(max_num * min_num)
 
+#25192
+import sys
+input = sys.stdin.readline
+ 
+N = int(input())
+ 
+names = set()
+cnt = 0
+ 
+for i in range(N):
+    name = input().strip()
+ 
+    if name == 'ENTER':
+        cnt += len(names)
+        names = set()
+    else:
+        names.add(name)
+ 
+cnt += len(names)
+print(cnt)
+
 #
