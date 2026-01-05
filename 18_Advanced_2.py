23453c60b1ccf55ea712f874fea1827e4cd5326f
Mingyu Kim <mattmingyu@gmail.com>
2026-01-05 21:17:21 +0900

    Update 18_Advanced_2.py

--- 18_Advanced_2.py
+++ 18_Advanced_2.py
@@ -29,4 +29,19 @@ for i in range(N):
 cnt += len(names)
 print(cnt)
 
+#26069
+N = int(input())
+dancePeople = {"ChongChong"}
+ 
+for _ in range(N):
+    A, B = input().split()
+ 
+    if A in dancePeople:
+        dancePeople.add(B)
+    if B in dancePeople:
+        dancePeople.add(A)
+ 
+print(len(dancePeople))
+
 #
+
