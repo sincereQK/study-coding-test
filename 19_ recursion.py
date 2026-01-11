424a297e8fc01644130f8d9845ba9c6013a085ad
Mingyu Kim <mattmingyu@gmail.com>
2026-01-11 23:55:51 +0900

    Update 19_ recursion.py

--- 19_ recursion.py
+++ 19_ recursion.py
@@ -10,4 +10,13 @@ def factorial(n):
 
 print(factorial(int(input())))
 
+#10870
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+n = int(input())
+print(fibonacci(n))
+
 #
