5b2355f8fe9220dc33029c8f3752b3e3db0bf333
Mingyu Kim <mattmingyu@gmail.com>
2026-02-25 23:37:29 +0900

    Add files via upload

---
+++ Java11/BOJ_2798.java
@@ -0,0 +1,43 @@
+package Bruteforcing;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class BOJ_2798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+
+                    if (sum == M) {
+                        System.out.println(sum);
+                        return;
+                    }
+
+                    if (sum < M && sum > result) {
+                        result = sum;
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
