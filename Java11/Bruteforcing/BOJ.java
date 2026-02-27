13fe2d4941f38fa322c9448def090d244917ca4c
Mingyu Kim <mattmingyu@gmail.com>
2026-02-27 23:58:29 +0900

    Update BOJ.java

--- Java11/Bruteforcing/BOJ.java
+++ Java11/Bruteforcing/BOJ.java
@@ -1,1 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
+public class BOJ_2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int result = 0; 
+        
+        for (int i = 1; i < N; i++) {
+            int number = i;
+            int sum = i;
+            
+            while (number > 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+            
+            if (sum == N) {
+                result = i;
+                break; 
+            }
+        }
+        
+        System.out.println(result);
+    }
+}
