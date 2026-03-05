f4fcbc777b695fa32403b2ff02e2619c3f566789
Mingyu Kim <mattmingyu@gmail.com>
2026-03-05 23:54:38 +0900

    Add files via upload

---
+++ Java11/Bruteforcing/BOJ_1436.java
@@ -0,0 +1,26 @@
+package Bruteforcing;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BOJ_1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int num = 666;
+        int count = 1;
+
+        while (count != N) {
+            num++;
+
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+
+        System.out.println(num);
+    }
+}
