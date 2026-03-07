8382731c186a0bef97232be68c0b4447a0ad0608
Mingyu Kim <mattmingyu@gmail.com>
2026-03-07 23:43:59 +0900

    Add files via upload

---
+++ Java11/Bruteforcing/BOJ_7568.java
@@ -0,0 +1,38 @@
+package Bruteforcing;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class BOJ_7568 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] person = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            person[i][0] = Integer.parseInt(st.nextToken());
+            person[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int rank = 1;
+
+            for (int j = 0; j < N; j++) {
+                if (person[i][0] < person[j][0] && person[i][1] < person[j][1]) {
+                    rank++;
+                }
+            }
+
+            sb.append(rank).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
