fda1ca0d319a8b8c9cae8faaf9730abaa08d5dbe
Mingyu Kim <mattmingyu@gmail.com>
2026-04-07 23:21:02 +0900

    [Bronze II] Title: 수 정렬하기, Time: 108 ms, Memory: 14564 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Arrays.sort(arr);
+        
+        StringBuilder sb = new StringBuilder();
+        for (int val : arr) {
+            sb.append(val).append("\n");
+        }
+        
+        System.out.print(sb);
+    }
+}
