e58691d39710ba7c5538d3ad69da8fea371bbd38
Mingyu Kim <mattmingyu@gmail.com>
2026-05-26 23:44:58 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 7.56 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String[] arr = s.split(" ");
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        // 배열을 돌며 최댓값과 최솟값 갱신
+        for (String str : arr) {
+            int number = Integer.parseInt(str);
+            
+            if (number < min) min = number;
+            if (number > max) max = number;
+        }
+        
+        return min + " " + max;
+    }
+}
