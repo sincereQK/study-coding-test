cbd4b00641e4da6d87f7c610c23c85ec58afd788
Mingyu Kim <mattmingyu@gmail.com>
2026-06-13 01:38:50 +0900

    [Passed] 길이 단위 환산하기 (148ms, 12MB)

---
+++ codetree/trail1/길이 단위 환산하기/convert-length-units.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        double N = sc.nextDouble();
+
+        double ft = 30.48;
+
+        System.out.printf("%.1f", N * ft);
+    }
+}
