3c2cdb7f759149c440c2bfc58a03abead5d250dd
Mingyu Kim <mattmingyu@gmail.com>
2026-06-13 01:30:44 +0900

    [Passed] 정수 입력받아 계산 (121ms, 10MB)

---
+++ codetree/trail1/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        a *= 2;
+        a += 3;
+
+        System.out.print(a);
+    }
+}
