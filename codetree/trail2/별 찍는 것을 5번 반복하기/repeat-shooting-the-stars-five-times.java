fbc870ce857a3a135b52faa6a83efe889522d7fb
Mingyu Kim <mattmingyu@gmail.com>
2026-06-13 01:50:51 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (91ms, 9MB)

---
+++ codetree/trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        for (int i = 0; i < 5; i++) {
+            print10Stars();
+        }
+    }
+    
+    public static void print10Stars() {
+        System.out.println("**********");
+    }
+}
